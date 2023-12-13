834363080b5c7edf9feeaacef937daa1fb1fc837
RNBSilva <RNBSilva@P381886>
2023-12-13 15:02:59 -0300

    Correct errors

--- secao4/core/configs.py
+++ secao4/core/configs.py
@@ -10,8 +10,8 @@ class Settings(BaseSettings):
     """
         Configurações gerais da API
     """
-    API_V1_STR: str = "/api/v1",
-    DB_URL: str = f"mysql+mysqlconnector://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@{os.getenv('MYSQL_HOST')}:3306/{os.getenv('MYSQL_DB')}"
+    API_V1_STR: str = "/api/v1"
+    DB_URL: str = f"mysql+aiomysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@{os.getenv('MYSQL_HOST')}:3306/{os.getenv('MYSQL_DB')}"
     DBBaseModel = declarative_base()
     
     class Config:
--- secao4/models/curso_model.py
+++ secao4/models/curso_model.py
@@ -3,7 +3,7 @@ from core.configs import settings
 from sqlalchemy import Column, Integer, String
 
 class CursoModel(settings.DBBaseModel):
-    __tableName__ = 'cursos'
+    __tablename__ = 'cursos'
     
     id: int = Column(Integer, primary_key=True, autoincrement=True)
     titulo: str = Column(String(100))
