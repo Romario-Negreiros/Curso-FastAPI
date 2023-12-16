1165bd85977a23a18e50fda27855908fc07352f0
Romario Negreiros <nromario482@gmail.com>
2023-12-16 03:35:20 -0300

    fix: correct package to import sqlalchemy session class

--- middlewares/core/database.py
+++ middlewares/core/database.py
@@ -1,14 +1,15 @@
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session as SyncSession
 from sqlalchemy import create_engine
 
 from core.configs import settings
 
 engine = create_engine(settings.DB_URL)
 
-Session = sessionmaker(
+Session: SyncSession = sessionmaker(
     autocommit       = False,
     autoflush        = False,
     expire_on_commit = False,
-    class_           = Session,
+    class_           = SyncSession,
     bind             = engine
 )
