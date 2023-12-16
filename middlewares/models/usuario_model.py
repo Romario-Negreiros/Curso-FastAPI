39b19d604ca2a5c1132b508d26bc5a5044705c87
Romario Negreiros <nromario482@gmail.com>
2023-12-15 21:06:30 -0300

    refactor: rename file, correct imports and make cpf column a primary_key and unique

--- middlewares/models/usuario_model.py
+++
@@ -1,9 +0,0 @@
-from fastapi import Integer, String, Column
-
-from core.configs import settings
-
-class UsuarioModel(settings.DBBaseModel):
-    __tablename__ = "usuarios"
-    
-    cpf = Column(Integer)
-    nome = Column(String)
